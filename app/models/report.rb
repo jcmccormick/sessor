beff38f9f5d8e13b7783691eee4f466c481895b4
jcmccormick <joe.c.mccormick@gmail.com>
2015-07-02 00:03:48 -0600

    Allows for spaces in report title regex

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,6 @@ class Report < ActiveRecord::Base
   belongs_to :template
   has_and_belongs_to_many :admins
   has_and_belongs_to_many :users
-  validates :title, uniqueness: true, format: { with: /\A[a-zA-Z0-9_]*[a-zA-Z][a-zA-Z0-9_]*\z/,
+  validates :title, uniqueness: true, format: { with: /\A[a-zA-Z0-9_]*[a-zA-Z][a-zA-Z0-9_ ]*\z/,
   	message: " must contain at least 1 letter and only letters and numbers" }
 end
