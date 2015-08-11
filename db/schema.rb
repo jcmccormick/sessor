651f8a77a398712d04d31333a5f5fd6f7a10f7ad
jcmccormick <joe.c.mccormick@gmail.com>
2015-08-10 22:19:51 -0600

    Migrates drop table sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808003410) do
+ActiveRecord::Schema.define(version: 20150811041417) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "provider",               limit: 255,                null: false
@@ -118,16 +118,6 @@ ActiveRecord::Schema.define(version: 20150808003410) do
 
   add_index "reports_users", ["report_id"], name: "index_reports_users_on_report_id", using: :btree
   add_index "reports_users", ["user_id"], name: "index_reports_users_on_user_id", using: :btree
-
-  create_table "sessions", force: :cascade do |t|
-    t.string   "session_id", limit: 255,   null: false
-    t.text     "data",       limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "templates", force: :cascade do |t|
     t.string   "name",          limit: 255
