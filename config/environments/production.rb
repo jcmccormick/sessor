a8b450077dac4cb98261b9d2563ce0b6b5abe12f
jcmccormick <joe.c.mccormick@gmail.com>
2015-11-23 13:44:45 -0800

    Revises production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,19 +35,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  # MEMCACHED RACK - CACHE
-
-  client = Dalli::Client.new((ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                           :username => ENV["MEMCACHIER_USERNAME"],
-                           :password => ENV["MEMCACHIER_PASSWORD"],
-                           :failover => true,
-                           :socket_timeout => 1.5,
-                           :socket_failure_delay => 0.2,
-                           :value_max_bytes => 10485760)
-  config.action_dispatch.rack_cache = {
-    :metastore    => client,
-    :entitystore  => client
-  }
   config.static_cache_control = "public, max-age=2592000"
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
