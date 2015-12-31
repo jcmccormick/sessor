77c259f605dc4f1f97519c2cf635cbc5da7c195b
jcmccormick <joe.c.mccormick@gmail.com>
2015-12-30 17:04:39 -0800

    Specifies dependency paths

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,8 +22,8 @@
 //= require angular-mocks/angular-mocks
 //= require angular-animate/angular-animate
 //= require angular-local-storage/dist/angular-local-storage.min
-//= require angular-material
-//= require angular-aria
+//= require angular-material/angular-material.js
+//= require angular-aria/angular-aria.js
 //= require angular-rails-templates
 //= require angular-cookie
 //= require angular-ui-sortable
