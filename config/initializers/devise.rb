679f84cef89feed12168bf68ba6de807f2419c49
Joe McCormick <joe.c.mccormick@gmail.com>
2016-01-25 03:10:29 -0800

    Removes development/ssl checks for omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,10 +4,5 @@ Devise.setup do |config|
     config.expire_all_remember_me_on_sign_out = true
     config.sign_out_via = :delete
     config.skip_session_storage = [:http_auth, :token_auth]
-
-    if Rails.env.development?
-        config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {:client_options => {:ssl => {:verify => false}}, skip_jwt: true, scope: "email, profile, https://www.googleapis.com/auth/drive, https://spreadsheets.google.com/feeds/", prompt: 'consent'}
-    else
-        config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {scope: "email, profile, https://www.googleapis.com/auth/drive, https://spreadsheets.google.com/feeds/", prompt: 'consent'}
-    end
+    config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {scope: "email, profile, https://www.googleapis.com/auth/drive, https://spreadsheets.google.com/feeds/", prompt: 'consent'}
 end
