8ebddcc0c797dcfbf46d632796a3ab17d397a4e1
Joe McCormick <joe.c.mccormick@gmail.com>
2016-02-01 15:05:07 -0800

    Adds https to Omniauth config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  OmniAuth.config.full_host = "http://www.clerkr.com"
+  OmniAuth.config.full_host = "https://www.clerkr.com"
 
 end
