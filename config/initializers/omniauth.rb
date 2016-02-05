eee64a9699942588af585513de8896441020e240
Joe McCormick <joe.c.mccormick@gmail.com>
2016-02-04 18:40:57 -0800

    Sets facebook image url to be called securely on heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']#, {scope: "email, profile, https://www.googleapis.com/auth/drive, https://spreadsheets.google.com/feeds/", access_type: 'offline', prompt: 'consent'}
     provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
-    provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET']
+    provider :facebook, ENV['FACEBOOK_CLIENT_ID'], ENV['FACEBOOK_CLIENT_SECRET'], :secure_image_url => !Rails.env.development?
 end
